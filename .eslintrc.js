f4de8b0302b682d0d247459968898d8cb894e1a7
Rodrigo facio <53882388+RodrigoFaccio@users.noreply.github.com>
2022-01-21 12:43:56 -0300

    add eslint and editor.config and pritter

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+  },
+  Option: {
+    'no-use-before-define': ['error', { functions: true, classes: true, variables: true }],
+  },
+};
